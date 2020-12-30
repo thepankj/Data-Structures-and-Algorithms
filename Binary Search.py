e9cf4ac72f5d5b7aeeddb8ad9441b091ff958fbc
Pankaj Gupta <62327263+thepankj@users.noreply.github.com>
2020-12-30 22:03:08 +0530

    Add files via upload

---
+++ Binary Search.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def binarySearch(arr, x):
+    start = 0
+    end = len(arr)
+    while start <= end:
+        mid = start + (end-start)//2
+        if x < arr[mid]:
+            end = mid - 1
+        elif x > arr[mid]:
+            start = mid + 1
+        else:
+            return True
+    return False
+
