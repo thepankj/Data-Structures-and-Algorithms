434c5f3b74645e9bee60752f8076cd1a876482ad
Pankaj Gupta <62327263+thepankj@users.noreply.github.com>
2021-03-07 15:12:19 +0530

    Add files via upload

---
+++ Online Judges/Hackerrank/Medium/Triangle Quest and Triangle Quest 2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[58]:
+
+
+#triangle quest
+for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print(((10**i)//9)*i)
+
+
+# In[59]:
+
+
+#triangle quest 2
+
+for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print(((10**i)//9)**2)
+
